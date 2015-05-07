ef965ea69c90e2a7dd7e8ae41f45989ad8169c0a
Brycen <dorsay@live.ca>
2015-05-07 12:21:10 -0700

    Prime number checking

--- app/app.js
+++ app/app.js
@@ -1,5 +1,22 @@
 var app = angular.module('spiral', []);
 
 app.controller('MainController', function($scope) {
-  $scope.test = 'test';
+  $scope.range = 10;
+
+  $scope.getNumber = function(num) {
+    return new Array(num);
+  };
+
+  $scope.checkPrime = function(num) {
+    if (num < 2) return false;
+
+    var q = parseInt(Math.sqrt(num));
+
+    for (var i = 2; i <= q; i++) {
+      if (num % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  };
 });
