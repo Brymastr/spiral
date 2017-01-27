bffe41aefa2716e98920dc415ebdfb5ea4bf86e6
Brycen Dorsay <dorsay@live.ca>
2017-01-27 15:41:35 -0800

    Resizable canvas with redraw

--- spiral.js
+++ spiral.js
@@ -1,10 +1,34 @@
-var canvas = document.getElementById('canvas');
+// Number of squares total
+const squares = 100;
+const dimension = Math.floor(Math.sqrt(squares));
 
-var ctx = canvas.getContext('2d');
+// Draw the canvas
+function draw() {
+  var ctx = document.getElementById('canvas').getContext('2d');
+  var screenWidth = window.innerWidth;
+  var screenHeight = window.innerHeight;
+  const spacing = 1.1;
+  ctx.canvas.width = ctx.canvas.height = screenWidth < screenHeight ? screenWidth : screenHeight
 
-ctx.fillStyle = 'green';
-for(j = 0; j < 100; j++) {
-  for(i = 0; i < 100; i++) {
-    ctx.fillRect(/* x position */ i * 10 * 2, /* y position */ j * 10 * 2, /* x length */ 10, /* y length */ 10);
+  var squareSize = (screenWidth < screenHeight ? screenWidth : screenHeight) / (dimension * spacing);
+  console.log(squareSize);
+  ctx.fillStyle = 'blue';
+  for(j = 0; j < dimension; j++) {
+    for(i = 0; i < dimension; i++) {
+      ctx.save();
+      ctx.translate(/* x position */ i * squareSize * spacing, /* y position */ j * squareSize * spacing)
+      ctx.fillRect(0, 0, /* x length */ squareSize, /* y length */ squareSize);
+      ctx.restore();
+    }
   }
+
 }
+
+var addEvent = function(object, type, callback) {
+  if(!object) return;
+  if(object.addEventListener) object.addEventListener(type, callback, false);
+  else if(object.attachEvent) object.attachEvent('on' + type, callback);
+  else object['on'+type] = callback;
+};
+window.onload = draw();
+addEvent(window, 'resize', draw);
