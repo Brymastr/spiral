2df763c2ac097289df770c0beded3ccfc706de05
Brycen <dorsay@live.ca>
2017-02-06 11:16:51 -0800

    More vue

--- spiral.js
+++ spiral.js
@@ -5,44 +5,66 @@ Vue.component('spiral', {
     </div>`
 });
 
+Vue.component('controls', {
+  props: ['color', 'squares', 'start'],
+  template: `
+    <div id="controls">
+      <input type="number" id="numberOfSquares" placeholder="Squares" v-bind:value="squares"/>
+      <input type="number" id="startingNumber" placeholder="Starting number"/>
+      <input type="text" id="squareColor" placeholder="Color"/>
+    </div>`,
+  data: function() {
+    return {}
+  }
+});
+
 // Vue.js
 var app = new Vue({
   el: '#spiral',
   data: function() {
     return {
-      color: 'blue',
-      primes: true,
-      width: 0,
-      ctx: null,
-      windowWidth: 0,
-      windowHeight: 0
+      params: {
+        color: null,
+        primes: true,
+        ctx: null,
+        width: 0,
+        height: 0,
+        squares: 10,
+        start: 1
+      }
     }
-    
   },
   methods: {
     drawCanvas: function() {
-      draw(this.color, this.ctx);
+      draw(this.params);
     },
-    getWindowWidth(event) {
-      this.windowWidth = document.documentElement.clientWidth;
+    getWindowWidth: function(event) {
+      this.params.width = window.innerWidth;
     },
-    getWindowHeight(event) {
-      this.windowHeight = document.documentElement.clientHeight;
+    getWindowHeight: function(event) {
+      this.params.height = window.innerHeight;
     }
   },
   watch: {
-    color: function(newVal, oldVal) {
-      this.ctx.fillStyle = newVal;
+    'params.color': function(newVal) {
+      this.params.ctx.fillStyle = newVal;
+      this.drawCanvas();
     },
-    windowWidth: function(newVal, oldVal) {
-      this.drawCanvas(this.color, this.ctx);
+    'params.width': function(newVal) {
+      this.drawCanvas();
     },
-    windowHeight: function(newVal, oldVal) {
-      this.drawCanvas(this.color, this.ctx);
+    'params.height': function(newVal) {
+      this.drawCanvas();
+    },
+    'params.squares': function(newVal) {
+      this.drawCanvas();
+    },
+    'params.start': function(newVal) {
+      this.drawCanvas();
     }
   },
   mounted: function() {
-    this.ctx = document.getElementById('canvas').getContext('2d');
+    this.params.ctx = document.getElementById('canvas').getContext('2d');
     this.$nextTick(function() {
       window.addEventListener('resize', this.getWindowWidth);
       window.addEventListener('resize', this.getWindowHeight);
@@ -60,58 +82,53 @@ var app = new Vue({
 
 
 // Draw the canvas
-function draw(color, ctx) {
-  // Number of squares total
-  const SQUARES = 9025;
+function draw(params) {
+  
+  // Parameters
+  const SQUARES = params.squares;
+  const screenWidth = params.width;
+  const screenHeight = params.height;
   const START = 1;
-  // Side dimensions if 
-  const DIMENSION = Math.ceil(Math.sqrt(SQUARES));
   const SHOW_NUMBERS = false;
   const PRIMES = true;
-  
-  const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight;
-  const spacing = 1;
-  var squareSize = (screenWidth < screenHeight ? screenWidth : screenHeight) / (DIMENSION * spacing);  
-  ctx.canvas.width = ctx.canvas.height = screenWidth < screenHeight ? screenWidth : screenHeight
+  const SPACING = 1;
+  const CANVAS = params.ctx;
+  const DIMENSION = Math.ceil(Math.sqrt(SQUARES));
+  var squareSize = (screenWidth < screenHeight ? screenWidth : screenHeight) / (DIMENSION * SPACING);
+  CANVAS.canvas.width = CANVAS.canvas.height = screenWidth < screenHeight ? screenWidth : screenHeight;
 
   var i = START;
   var increment = 1;
   var direction = 0;
-  var xTranslation = [1, 0, -1, 0];
-  var yTranslation = [0, -1, 0, 1];
+  const xTranslation = [1, 0, -1, 0];
+  const yTranslation = [0, -1, 0, 1];
 
-  ctx.fillStyle = color;
+  CANVAS.fillStyle = params.color;
 
-  ctx.translate((DIMENSION - 1) / 2 * (spacing * squareSize), (DIMENSION - 1) / 2 * (spacing * squareSize))
-  if(SHOW_NUMBERS) ctx.fillText(i, 0, 0);
-  ctx.fillRect(0, 0, squareSize, squareSize);  
+  CANVAS.translate((DIMENSION - 1) / 2 * (SPACING * squareSize), (DIMENSION - 1) / 2 * (SPACING * squareSize));
+  if(SHOW_NUMBERS) CANVAS.fillText(i, 0, 0);
+  CANVAS.fillRect(0, 0, squareSize, squareSize);
   ++i;
+
   maxSquares:
-  while(1) {                                 // Total number of squares
+  while(1) {                            // Total number of squares
     for(j = 0; j < 2; j++) {            // Change direction every two loops of j
       for(k = 0; k < increment; k++) {  // increases by one every two loops of j
-        translate = {x: xTranslation[direction % 4] * squareSize * spacing, y: yTranslation[direction % 4] * squareSize * spacing}
-        ctx.save();
-        ctx.translate(translate.x, translate.y);
-        PRIMES ? isPrime(i) && ctx.fillRect(0, 0, squareSize, squareSize) : ctx.fillRect(0, 0, squareSize, squareSize);
-        if(SHOW_NUMBERS) ctx.fillText(i, 0, 0);
-        i++;
-        if(i > SQUARES) break maxSquares;
+        translate = {x: xTranslation[direction % 4] * squareSize * SPACING, y: yTranslation[direction % 4] * squareSize * SPACING};
+        CANVAS.save();
+        CANVAS.translate(translate.x, translate.y);
+        PRIMES ? isPrime(i) && CANVAS.fillRect(0, 0, squareSize, squareSize) : CANVAS.fillRect(0, 0, squareSize, squareSize);
+        if(SHOW_NUMBERS) CANVAS.fillText(i, 0, 0);
+        if(++i > SQUARES) break maxSquares;
       }
-      
       direction++;
     }
     increment++;
   }
-
 }
 
 function isPrime(number) {
-  for(var i = 2; i < number; i++) {
-    if(number % i === 0) {
-      return false;
-    }
-  }
+  for(var i = 2; i < number; i++)
+    if(number % i === 0) return false;
   return number > 1;
 }
